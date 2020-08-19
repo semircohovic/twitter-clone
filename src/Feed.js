a1f647a153c365585d3c143eaa36d6d544c11a4d
Semir Cohovic <semircohovic@gmail.com>
2020-08-19 16:48:13 +0200

    post init

--- src/Feed.js
+++ src/Feed.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import TweetBox from './TweetBox';
+import Post from './Post';
 import './Feed.css';
 function Feed() {
     return (
@@ -8,6 +9,8 @@ function Feed() {
                 <h2>Home</h2>
             </div>
             <TweetBox />
+            <Post />
+
         </div>
     )
 }
